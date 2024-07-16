3508df05325986d36e07d57858d9a76ff58f20d2
Ifra732 <ifrariaz732@gmail.com>
2024-07-15 21:26:46 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+//for file exicution
+//SHABANG
+import inquirer from "inquirer";
+
+
+let userprompt = await inquirer.prompt({
+    name:"usernumber",
+    type:"number",
+    message:"enter your number between 1 to 10 !"
+})
+console.log("usernumber",userprompt.usernumber);
+
+let computernumber :number = Math.floor(Math.random() * 10)
+console.log("computernumber",computernumber);
+
+if(userprompt.usernumber === computernumber){
+    console.log("congratulation!you won the game");
+    
+}else{
+    console.log("ohh! you lose the game");
+    
+}
